c71cb759d92cf5b37fb4cd34a406a58a05cb0eb6
echo000 <08finncam@gmail.com>
2024-05-27 20:20:16 +0100

    .

--- lua/csharp/features/debugger/config_factories/launch-debugger.lua
+++ lua/csharp/features/debugger/config_factories/launch-debugger.lua
@@ -12,6 +12,7 @@ local function create_config(args)
     logger.debug("Skip debugging, build failed!", { feature = "debugger" })
     error("Skip debugging, build failed!")
   end
+  print(args.project_information)
 
   return {
     name = "Launch - .NET",
